074f3fe4aec09dffcdf5415f2b6268501823addb
scottrosoff <srosoff@enotes.com>
2017-11-04 14:48:13 -0700

    Update impersonateTagAttack.m

--- impersonateTagAttack.m
+++ impersonateTagAttack.m
@@ -1,6 +1,13 @@
+% Scott Rosoff
+
 SMA = SMAuthProtocol();
 
 %Your code goes here: simulate the tag impersonation attack of Cai,
 %Li, Li, and Deng
+
+% first, adversary generates random string r1' and sends it to tag
+r1_prime = 
+
+
 [SMA, accept] = continueAsTag(SMA, xorLongString(M1_prime, xorLongString(r1, r1_prime)), M2_prime);
 %Code written after the call to impersonateTag will be DELETED
