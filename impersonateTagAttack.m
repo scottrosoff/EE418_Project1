ed948a51ecc175ac6a51ae5163d384515fbefc69
scottrosoff <srosoff@enotes.com>
2017-11-06 11:58:26 -0800

    Update impersonateTagAttack.m

--- impersonateTagAttack.m
+++ impersonateTagAttack.m
@@ -10,14 +10,14 @@ SMA = SMAuthProtocol();
 % first, initiate tag as an adversary 
 [SMA, r1_prime] = initiateAsTag(SMA);
 % send r1' to the tag
-SMA.r1 = r1_prime;
+%SMA.r1 = r1_prime;
 
 % tag generates random string r2
 r2 = generateRandBitString(SMA.l);
 
 % find M1' and M2'
 M1_prime = xorLongString(SMA.t, r2);
-temp = [SMA.t xorLongString(r1_prime, r2)
+temp = [SMA.t xorLongString(r1_prime, r2)];
 M2_prime = binHash(temp);
 
 % Send M1' and M2' to adversary
